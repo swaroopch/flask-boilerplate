74e79895d02c01744032cec2818eb276c1fe6148
Swaroop C H <swaroop@swaroopch.com>
2010-08-30 18:54:22 +0530

    Fixing inits

--- flask_application/__init__.py
+++ flask_application/__init__.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python
-# coding=utf-8
 
-import sys
-assert (2,6) <= sys.version_info < (3,0)
-
-import datetime
 from flask import Flask
-import config
+from flask_application.controllers.frontend import frontend
 
 app = Flask(__name__)
-app.secret_key = '{SECRET_KEY}'
-
-log = config.logger(config.SITE_NAME)
-
-import flask_application.controllers
+app.register_module(frontend)
 
--- flask_application/controllers/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-from flask import Flask
-from flask_application.controllers.frontend import frontend
-
-app = Flask(__name__)
-app.register_module(frontend)
-
