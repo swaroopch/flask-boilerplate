d37b400654d7dff6ba06dad7795c4006521f9206
Swaroop C H <swaroop@swaroopch.com>
2010-08-30 19:29:03 +0530

    Adding back secret_key declaration

--- flask_application/__init__.py
+++ flask_application/__init__.py
@@ -6,6 +6,8 @@ from flask_application.controllers.frontend import frontend
 import config
 
 app = Flask(__name__)
+app.secret_key = '{SECRET_KEY}'
+
 # http://flask.pocoo.org/docs/patterns/packages/
 app.register_module(frontend)
 
