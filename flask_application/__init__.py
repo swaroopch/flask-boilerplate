5bdfe2d97678535f51c6ee39fe69a84a4e3d7f5d
Swaroop C H <swaroop@swaroopch.com>
2010-08-30 19:30:51 +0530

    Minor refactoring.

--- flask_application/__init__.py
+++ flask_application/__init__.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 
 from flask import Flask
-from flask_application.controllers.frontend import frontend
-
-import config
 
 app = Flask(__name__)
 app.secret_key = '{SECRET_KEY}'
 
 # http://flask.pocoo.org/docs/patterns/packages/
+from flask_application.controllers.frontend import frontend
 app.register_module(frontend)
 
