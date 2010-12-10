2f805a450af2d26ce8bde8a7dff138b80cbce026
Swaroop C H <swaroop@swaroopch.com>
2010-12-10 09:31:38 +0530

    Rewriting how code has synced to the server

--- fabfile.py
+++ fabfile.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python
 
-from fabric.api import env, local, put, cd, run, sudo
+'''\
+Run 'fab --list' to see list of available commands.
+
+References:
+# http://docs.fabfile.org/0.9.3/usage/execution.html#how-host-lists-are-constructed
+'''
+
+import os
+import platform
+
+from fabric.api import hosts, local, sudo
 from fabric import colors
 
-# NOTE Add host names here so that you do not have to specify it every time on the command line
-# See http://docs.fabfile.org/0.9.3/usage/execution.html#how-host-lists-are-constructed
-# example: env.hosts.extend(['user@server1:port', 'user@server2:port']) # where port is the SSH port
-env.hosts.extend([])
+def transfer_files(src, dst):
+    assert os.getenv('SSH_AUTH_SOCK') is not None # Ensure ssh-agent is running
+    assert src.endswith('/')
+    assert not dst.endswith('/')
+    local('rsync -avh --delete-before --copy-unsafe-links -e ssh {0} {1}'.format(src, dst), capture=False)
 
-SITE_NAME = '{SITE_NAME}'
 
-def deploy():
-    global SITE_NAME
+## TODO This is only an example. Change this according to your specific servers setup.
+@hosts('domain.com')
+def laptop_to_server():
+    '''Deploys the code from the laptop to the server'''
+    assert platform.system() == 'Darwin' # Ensures that I run this only on the laptop (Darwin == Mac OS X)
+    local_dir = '/Users/swaroop/code/domain.com/'
+    remote_dir = 'domain.com:/home/swaroop/web/domain.com/private/domain.com'
+    transfer_files(local_dir, remote_dir)
+    sudo('apache2ctl graceful')
+    print(colors.magenta('Success! The server has been updated.'))
 
-    # Zip current directory
-    local('rm -f /tmp/{0}.zip'.format(SITE_NAME))
-    local('zip -rq /tmp/{0}.zip .'.format(SITE_NAME))
-    # Copy to server
-    run('rm -f /tmp/{0}.zip'.format(SITE_NAME))
-    put('/tmp/{0}.zip'.format(SITE_NAME), '/tmp/')
-    run('mkdir -p $HOME/web/{0}'.format(SITE_NAME))
-    with cd('$HOME/web/{0}'.format(SITE_NAME)):
-        run('unzip /tmp/{0}.zip'.format(SITE_NAME))
-    # Cleanup
-    run('rm -f /tmp/{0}.zip'.format(SITE_NAME))
-    local('rm -f /tmp/{0}.zip'.format(SITE_NAME))
-    # Restart server
-    sudo('apache2ctl restart')
-    print(colors.magenta('Success! The website code has been updated.'))
 
